e02c04917c2999a54b33c41a1021d1f134768b3b
abhishekmind <abhishek.singh1@mind-infotech.com>
2019-08-05 18:21:06 +0530

    Upload code

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+import postReducer from './reducer/postReducer';
+
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const store = createStore(postReducer);
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>, 
+	document.getElementById('root')
+);
